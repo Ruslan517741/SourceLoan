b4c5865be0603c1eb4a683adb45f5d98b4ff6e38
Rus <mamail>
2021-08-26 14:16:40 +0300

    new slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,6 +15,21 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(e) {}
+        
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
@@ -27,13 +42,17 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e){}
+        
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
             });
 
             item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault;
+                e.preventDefault();
                 this.slideIndex = 1;
                 this.showSlides(this.slideIndex);
             });
